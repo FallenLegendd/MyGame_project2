713e493b4ed6fc330f2258ed48cacd36455bf2c8
Saveliy <borisov.saveliy@mail.ru>
2025-07-17 23:12:03 +0700

    user on server is ready

--- code/server/src/routes/indexRouter.js
+++ code/server/src/routes/indexRouter.js
@@ -3,7 +3,9 @@ const formatResponse = require("../utils/formatResponse");
 const questionRouter = require("./questionRouter");
 const gameRouter = require('./game.router');
 const answerRouter = require('./answer.route')
+const authRouter = require('./auth.routes');
 
+router.use("/auth", authRouter);
 router.use("/question", questionRouter);
 router.use("/game", gameRouter);
 router.use("/answer", answerRouter);
